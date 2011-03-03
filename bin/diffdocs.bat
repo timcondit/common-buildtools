7711b502133bd82678f956c11fd906a30d08865a
Tim Condit <timc@envisioninc.com>
2011-03-02 17:11:37 -0800

    comments in diffdocs and skip lkg in build.xml

--- bin/diffdocs.bat
+++ bin/diffdocs.bat
@@ -26,26 +26,30 @@ set DIFF=diff --brief
 :: EN CHM
 %DIFF% "%SRC%\EN\CHM files\Administrator_Help.chm"      "%DST%\OnlineHelp\EN\Administrator_Help.chm"
 %DIFF% "%SRC%\EN\CHM files\eLearning_Help.chm"          "%DST%\OnlineHelp\EN\eLearning_Help.chm"
+:: TODO remove EWFM.chm in Charlie
 %DIFF% "%SRC%\EN\CHM files\EWFM.chm"                    "%DST%\OnlineHelp\EN\EWFM.chm"
 %DIFF% "%SRC%\EN\CHM files\QualityMonitoring_Help.chm"  "%DST%\OnlineHelp\EN\QualityMonitoring_Help.chm"
 :: EN PDF
-:: TODO rename QRS-CentricityAgentDesktop.pdf to "QRS - CentricityAgentDesktop.pdf"?
 %DIFF% "%SRC%\EN\PDF files\QRS-CentricityAgentDesktop.pdf"  "%DST%\QuickReference\QRS - CentricityAgentDesktop.pdf"
 %DIFF% "%SRC%\EN\PDF files\ReleaseNotes.pdf"                "%DST%\ReleaseNotes\ReleaseNotes.pdf"
-:: TODO after 9.8 - uppercase administrator.pdf
 %DIFF% "%SRC%\EN\PDF files\administrator.pdf"               "%DST%\UserGuides\administrator.pdf"
+:: TODO remove AgentDesktop.pdf in Charlie
 %DIFF% "%SRC%\EN\PDF files\AgentDesktop.pdf"                "%DST%\UserGuides\AgentDesktop.pdf"
+:: TODO remove BizIntelPortal.pdf in Charlie?
 %DIFF% "%SRC%\EN\PDF files\BizIntelPortal.pdf"              "%DST%\UserGuides\BizIntelPortal.pdf"
 %DIFF% "%SRC%\EN\PDF files\eLearning.pdf"                   "%DST%\UserGuides\eLearning.pdf"
 %DIFF% "%SRC%\EN\PDF files\EnvisionCallTaggingClient.pdf"   "%DST%\UserGuides\EnvisionCallTaggingClient.pdf"
+:: TODO remove EWFM.pdf in Charlie
 %DIFF% "%SRC%\EN\PDF files\EWFM.pdf"                        "%DST%\UserGuides\EWFM.pdf"
 %DIFF% "%SRC%\EN\PDF files\QualityMonitoring.pdf"           "%DST%\UserGuides\QualityMonitoring.pdf"
-:: TODO after 9.8 - uppercase reporting.pdf
+:: TODO remove reporting.pdf in Charlie
 %DIFF% "%SRC%\EN\PDF files\reporting.pdf"                   "%DST%\UserGuides\reporting.pdf"
 :: EN HTML
+:: TODO remove Agent_Desktop_Webhelp.zip in Charlie
 %DIFF% "%SRC%\EN\Webhelp files\Agent_Desktop_Webhelp.zip"               "%DST%\Webhelp\EN\Agent_Desktop_Webhelp.zip"
 %DIFF% "%SRC%\EN\Webhelp files\BI_Webhelp.zip"                          "%DST%\Webhelp\EN\BI_Webhelp.zip"
 %DIFF% "%SRC%\EN\Webhelp files\Centricity_Webhelp.zip"                  "%DST%\Webhelp\EN\Centricity_Webhelp.zip"
+:: TODO remove Classic_reports_Webhelp.zip in Charlie
 %DIFF% "%SRC%\EN\Webhelp files\Classic_reports_Webhelp.zip"             "%DST%\Webhelp\EN\Classic_reports_Webhelp.zip"
 %DIFF% "%SRC%\EN\Webhelp files\Agent_Desktop_Centricity_Webhelp.zip"    "%DST%\Webhelp\EN\Agent_Desktop_Centricity_Webhelp.zip"
 
@@ -54,8 +58,10 @@ set DIFF=diff --brief
 %DIFF% "%SRC%\DE\CHM files\eLearning_Help.chm"          "%DST%\OnlineHelp\DE\eLearning_Help.chm"
 %DIFF% "%SRC%\DE\CHM files\QualityMonitoring_Help.chm"  "%DST%\OnlineHelp\DE\QualityMonitoring_Help.chm"
 :: DE HTML
+:: TODO remove Agent_Desktop_Webhelp.zip in Charlie
 %DIFF% "%SRC%\DE\Webhelp files\Agent_Desktop_Webhelp.zip"   "%DST%\Webhelp\DE\Agent_Desktop_Webhelp.zip"
 %DIFF% "%SRC%\DE\Webhelp files\Centricity_Webhelp.zip"      "%DST%\Webhelp\DE\Centricity_Webhelp.zip"
+:: TODO remove Classic_reports_Webhelp.zip in Charlie
 %DIFF% "%SRC%\DE\Webhelp files\Classic_reports_Webhelp.zip" "%DST%\Webhelp\DE\Classic_reports_Webhelp.zip"
 
 :: ES CHM
@@ -63,8 +69,10 @@ set DIFF=diff --brief
 %DIFF% "%SRC%\ES\CHM files\eLearning_Help.chm"          "%DST%\OnlineHelp\ES\eLearning_Help.chm"
 %DIFF% "%SRC%\ES\CHM files\QualityMonitoring_Help.chm"  "%DST%\OnlineHelp\ES\QualityMonitoring_Help.chm"
 :: ES HTML
+:: TODO remove Agent_Desktop_Webhelp.zip in Charlie
 %DIFF% "%SRC%\ES\Webhelp files\Agent_Desktop_Webhelp.zip"   "%DST%\Webhelp\ES\Agent_Desktop_Webhelp.zip"
 %DIFF% "%SRC%\ES\Webhelp files\Centricity_Webhelp.zip"      "%DST%\Webhelp\ES\Centricity_Webhelp.zip"
+:: TODO remove Classic_reports_Webhelp.zip in Charlie
 %DIFF% "%SRC%\ES\Webhelp files\Classic_reports_Webhelp.zip" "%DST%\Webhelp\ES\Classic_reports_Webhelp.zip"
 
 
